376b58ca3f160094ffab177f8788caf24a1b9501
TLarsh <adeyemisam2015@gmail.com>
2026-05-20 06:53:03 +0100

    feat: implement RateLimitedUI component and integrate it into HomePage

--- frontend/src/pages/homePage.jsx
+++ frontend/src/pages/homePage.jsx
@@ -1,9 +1,31 @@
+import { useEffect } from "react";
 import Navbar from "../components/Navbar";
+import RateLimitedUI from "../components/RateLimitedUI";
 
 const HomePage = () => {
+  const [isRateLimited, setIsRateLimited] = useState(true);
+  const [notes, setNotes] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() =>{
+    const fetchNotes = async () => {
+      try {
+        const response = await fetch("/api/notes");
+        const data = await response.json();
+        setNotes(data);
+      } catch (error) {
+        console.error("Error fetching notes:", error);
+      } finally {
+        setLoading(false);
+      }
+    }
+    fetchNotes(); 
+  }, [])
+
   return (
     <div className="min-h-screen">
       <Navbar/>
+      { isRateLimited && <RateLimitedUI /> }
     </div>
   )
 }
