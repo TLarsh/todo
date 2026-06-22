9875abc61b8d387f5f910af0eb17b44b03adef43
TLarsh <adeyemisam2015@gmail.com>
2026-06-22 18:13:03 +0100

    fix: correct import statement and update heading styles in NotesNotFound component

--- frontend/src/components/NotesNotFound.jsx
+++ frontend/src/components/NotesNotFound.jsx
@@ -1,13 +1,14 @@
-import { NotebookIcon } from "lucid-react";
-import
+import { NotebookIcon } from "lucide-react";
+import { Link } from "react-router";
+
 const NotesNotFound = () => {
     return (
         <div className="flex flex-col items-center justify-center py-16 space-y-6 max-w-md mx-auto text-center">
             <div className="bg-primary/10 rounded-full p-8">
                 <NotebookIcon className="size-10 text-primary" />
             </div>
-            <h2 className="text-xl font-semibold text-gray-800">No notes yet</h2>
-            <p className="text-gray-600">There are no notes in the database. Create your first note to get started.</p>
+            <h3 className="text-xl font-bold ">No notes yet</h3>
+            <p className="text-base-content/70">There are no notes in the database. Create your first note to get started.</p>
             <button className="mt-4 px-4 py-2 bg-blue-600 text-white rounded-md">
                 Create note
             </button>
