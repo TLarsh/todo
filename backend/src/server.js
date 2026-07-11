970d2a83fffcd54a975eff439565fe5342f5a775
TLarsh <adeyemisam2015@gmail.com>
2026-07-11 12:15:48 +0100

    fix: refactor CORS and static file serving logic in server.js for production environment

--- backend/src/server.js
+++ backend/src/server.js
@@ -13,19 +13,23 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 const __dirname = path.resolve()
 
-app.use(cors({
-  origin: "http://localhost:5173",
-}));
+if (process.env.NODE_ENV !== "production") {
+  app.use(cors({
+    origin: "http://localhost:5173",
+  }));
+}
 
 app.use(express.json());
 app.use(rateLimiter);
 
 // connectDb();
 app.use("/api/notes", notesRoutes);
-app.use(express.static(path.join(__dirname, "../frontend/dist")))
-app.use("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "../frontend/"))
-})
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../frontend/dist")));
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+  });
+}
 
 // // Simple CORS middleware for local development (replace with `cors` in production)
 // app.use((req, res, next) => {
